1364bf61d5e651f58c22fa420ae4550410545046
gtuchilus <gtuchilus@gmail.com>
2023-11-25 21:45:01 +0200

    test case

--- scheduled_tweets/test/application_system_test_case.rb
+++ scheduled_tweets/test/application_system_test_case.rb
@@ -6,3 +6,6 @@ end
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1200, 1200]
 end
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :chrome, screen_size: [1000, 1000]
+end
