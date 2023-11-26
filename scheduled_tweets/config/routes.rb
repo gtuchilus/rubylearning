8a875dc6c94b6cde20a83cc55614bd843935c2c5
gtuchilus <gtuchilus@gmail.com>
2023-11-26 02:19:33 +0200

    Part 13: Creating a Sign Up Form

--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "main#index"
 
   # Get /about
   get "about", to: "about#index", as: :about
 
-  # Get "/" homepage
-  root to: "main#index"
+  # Sign-up page
+  get "sign_up", to:"registrations#new"
+  post "sign_up", to:"registrations#create"
 end
