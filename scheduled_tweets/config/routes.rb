6131d16da9ddf331a065ea61b9708b2c555382a7
gtuchilus <gtuchilus@gmail.com>
2023-11-28 21:34:44 +0200

    Part 21: Reset Password Token Mailer

--- scheduled_tweets/config/routes.rb
+++ scheduled_tweets/config/routes.rb
@@ -29,5 +29,6 @@ Rails.application.routes.draw do
   # Password reset
   get "password/reset", to: "password_resets#new"
   post "password/reset", to: "password_resets#create"
-
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 end
